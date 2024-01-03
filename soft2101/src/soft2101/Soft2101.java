59999df5ce5b0dff932f55cc9900ed99ff1bd235
erdemonal11 <137915983+erdemonal11@users.noreply.github.com>
2024-01-03 22:28:21 +0300

    Initial commit
    satisfying requirement one

---
+++ soft2101/src/soft2101/Soft2101.java
@@ -0,0 +1,32 @@
+package soft2101;
+
+import java.util.Scanner;
+
+public class Soft2101 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int secondNumber = scanner.nextInt();
+
+        scanner.close();
+
+     
+        if (firstNumber < secondNumber) {
+            for (int i = firstNumber + 1; i < secondNumber; i++) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("Invalid input. The first number should be less than the second number.");
+        }
+    }
+}
+
+
+    
+
